df32e087b2531dd5a399c07512643516052d17ff
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 13:15:58 +0200

    Fix

--- nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
+++ nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
@@ -1,21 +1,6 @@
 namespace nunit_csharp_samples.Tests;
 
-public class LibraryStatisticsAnalyzer
+public class LibraryStatisticsAnalyzerTests
 {
-    public List<Book> GetBooksByAuthor(string author)
-    {
-
-        return Books.Where(b => b.Author == author).ToList();
-    }
-
-    public Book GetBookByTitle(string title)
-    {
-
-        return Books.FirstOrDefault(b => b.Title == title);
-    }
-
-    public Book GetBookByArticul(string articul)
-    {
-        return Books.FirstOrDefault(b => b.Articul == articul);
-    }
-} }
+    // TODO step 4.
+}
