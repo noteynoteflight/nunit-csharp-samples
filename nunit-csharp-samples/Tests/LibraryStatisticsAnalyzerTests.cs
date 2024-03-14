9c23be045bd404bf38accb910ed639ed11fb0e59
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 13:12:23 +0200

    Added unit tests

--- nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
+++ nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
@@ -1,6 +1,25 @@
 namespace nunit_csharp_samples.Tests;
 
+using NUnit.Framework;
+
+[TestFixture]
 public class LibraryStatisticsAnalyzerTests
 {
-    // TODO step 4.
-}
+    [Test]
+    public void GetBooksByAuthor_ReturnsCorrectBooks()
+    {
+
+        var analyzer = new LibraryStatisticsAnalyzer();
+        var book1 = new Book { Title = "Book 1", Author = "Author 1", Articul = "12345" };
+        var book2 = new Book { Title = "Book 2", Author = "Author 2", Articul = "67890" };
+        analyzer.Books.AddRange(new List<Book> { book1, book2 });
+
+
+        var result = analyzer.GetBooksByAuthor("Author 1");
+
+
+        Assert.AreEqual(1, result.Count);
+        Assert.Contains(book1, result);
+    }
+
+} }
