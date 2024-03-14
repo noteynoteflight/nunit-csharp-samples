1bb5c33e97ae26e5ee6998fab67363652b02d06a
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 13:20:06 +0200

    fix

--- nunit-csharp-samples/Interfaces/ILibraryManager.cs
+++ nunit-csharp-samples/Interfaces/ILibraryManager.cs
@@ -3,31 +3,15 @@
 /// </summary>
 public interface ILibraryManager
 {
-    public class Book
-    {
-        public string Articul { get; set; }
-    }
     /// <summary>
     /// Adds a book to the library's collection.
     /// </summary>
     /// <param name="book">The book to add.</param>
-    public void AddBook(Book book)
-    {
-        if (Books.Any(b => b.Articul == book.Articul))
-        {
-            Console.WriteLine("Error: Book with the same articul already exists.");
-            return;
-        }
-
-        Books.Add(book);
-    }
+    void AddBook(Book book);
 
     /// <summary>
     /// Removes a book from the library's collection.
     /// </summary>
     /// <param name="book">The book to remove.</param>
-    public bool RemoveBook(Book book)
-    {
-        return Books.Remove(book);
-    }
+    void RemoveBook(Book book);
 }
